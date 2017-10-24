c4f0fde84b572bc70d77a9cd276c138b2767e182
Catherine Etter <cetter.catherine.web@gmail.com>
2017-10-24 16:20:02 -0500

    Added Hello World in C and updated contributions

---
+++ hello_world/hello_world_CatherineEtter.c
@@ -0,0 +1,12 @@
+// ===============================
+// = LANGUAGE: C
+// = AUTHOR: Catherine Etter
+// = GITHUB: https://github.com/CatherineEtter
+// ===============================
+
+#include <stdio.h>
+
+int main(void)
+{
+   printf("Hello World!");
+}
