983e673ab0123c46a95c10a70562e3323f9611ac
Draem <owen@gimbel.ca>
2017-10-24 17:03:34 -0400

    Hello World in JAVA

---
+++ hello_world/hello_world.java
@@ -0,0 +1,11 @@
+public class Example {
+
+  /**
+  * Prints "Hello World" to the terminal.
+  */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+  
+  
+}
