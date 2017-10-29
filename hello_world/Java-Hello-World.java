61fdadc089dc8600b3ff34801c79e344306ea762
THSupun <thilinasupunsara@gmail.com>
2017-10-29 18:41:28 +0530

    Added a hello world script in java

--- hello_world/Java-Hello-World.java
+++ hello_world/Java-Hello-World.java
@@ -1,3 +1,6 @@
+// LANGUAGE: java
+// AUTHOR: Thilina
+// GITHUB: https://github.com/thilina999
 public class helloworld {
     public static void main(String[]args) {
         System.out.println("  Hello World !!!  ");
