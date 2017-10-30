60e73c04d17f155ac8c05f80b2927ab56d499bb2
sangeeth_ <32442096+sangeeth143k@users.noreply.github.com>
2017-10-30 17:12:37 +0530

    Hello world In Java

---
+++ java/hello_world_Sangeeth143k.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Sangeeth
+ * https://github.com/sangeeth143k/
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello World");
+        
+        
+    }
+}
