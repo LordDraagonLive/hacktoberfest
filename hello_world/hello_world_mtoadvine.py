c8fd81256b81e97b3f1e01985008b1aeaaa53842
Matthew Toadvine <toad1225@gmail.com>
2018-10-05 10:21:19 -0400

    Create hello_world_mtoadvine.py

---
+++ hello_world/hello_world_mtoadvine.py
@@ -0,0 +1,6 @@
+// LANGUAGE: python 
+// ENV: virtualenv 
+// AUTHOR: mtoadvine 
+// GITHUB: https://github.com/mtoadvine 
+print ('Hello, World!')
+print ('I wanted to add something different than just Hello World')
