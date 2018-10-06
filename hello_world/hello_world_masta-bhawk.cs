797bd80547d06402a621cfa9fd147608dc3a08f8
masta-bhawk <josh@hindmon.net>
2018-10-06 14:33:47 -0500

    basic hello world in c#

---
+++ hello_world/hello_world_masta-bhawk.cs
@@ -0,0 +1,17 @@
+// LANGUAGE: c#
+// ENV: 
+// AUTHOR: masta bhawk
+// GITHUB: https://github.com/masta-bhawk
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello World!");
+        }
+    }
+}
