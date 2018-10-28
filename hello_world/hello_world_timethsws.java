eced7a5cbeaeeed242b8fcb6e71bfc2b450491d5
Timeth Subasinghe <timethsws@gmail.com>
2018-10-28 18:52:44 +0530

    Add contributor and hello world

---
+++ hello_world/hello_world_timethsws.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// ENV: jvm
+// AUTHOR: Timeth Subasinghe
+// GITHUB: https://github.com/timethsws
+
+public class HelloWorld {
+  public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+ }
