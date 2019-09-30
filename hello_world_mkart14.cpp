9b5bece031bd1e7f54c0a0ee7d3be5dee37917a9
mkart14 <paras.kartikey@gmail.com>
2019-10-01 02:23:18 +0530

    Hello World in C++

---
+++ hello_world_mkart14.cpp
@@ -0,0 +1,11 @@
+/* LANGUAGE: c++ ENV: virtualenv AUTHOR: Kartikay Gupta GITHUB: https://github.com/mkart14 */
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   cout<<"Hello World!";
+
+   return 0;
+}
